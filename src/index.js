52650a7f59e4c6156922e84321ea7667034ef121
mubarak23 <mubarakaminu340@gmail.com>
2021-07-06 15:08:00 +0100

    setup auth0 flow Auth0Provider

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css';
-import { Provider } from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-import {configureStore} from './store/configureStore';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "semantic-ui-css/semantic.min.css";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "./store/configureStore";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App/>
+      <Auth0Provider>
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
-  </Provider>, document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
